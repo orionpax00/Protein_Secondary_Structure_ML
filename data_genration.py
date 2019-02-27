1bfd8fcdda64323183425bec376a7d15fb9eaca7
durgesh123-iitr <durgesh123.iitr@gmail.com>
2019-02-27 17:09:28 +0530

    clastalo added

--- data_genration.py
+++ data_genration.py
@@ -16,6 +16,56 @@ with open('d_tmp.fasta','w') as d_tmp:
         ini = infile.read().replace('H','G').replace('E','A')
         d_tmp.write(ini)
 
+with open('./svm_fasta','w') as svm_fas:
+    with open(sys.argv[2], "rb") as infile:
+        ini = infile.read().replace('H','G').replace('E','A')
+        svm_fas.write(ini)
+    svm_fas.write(">query.d\n")
+    with open("seq.sec_str", "rb") as infile:
+        ini = infile.read().replace('H','G').replace('E','A')
+        svm_fas.write(ini)
+
+os.system('/home/deepak/durgeshapi/Protein_Secondary_Structure_ML/clustalo'+' -i ./svm_fasta -o'+ ' ./output.txt'+ ' --outfmt'+ ' vie' + ' --force')
+
+with open('svm_secondary_structure.train','w') as svm_sec_str:
+    string = ""
+    with open('output.txt','r') as clastalo_out:
+        lines = clastalo_out.readlines()
+        super_string = ""
+        for counter,line in enumerate(lines):
+            if counter == 0:
+                continue
+            elif (counter) % 2 != 0:
+                if lines[counter-1][1:lines[counter-1].index('.')+2] in positive_fasta:
+                    sub_string = "+1 "
+                else:
+                    sub_string = "-1 "
+                line = line.replace('A','C').replace('G','H')
+                for counter2,letter in enumerate(line):
+                    if letter == 'H':
+                        sub_string += str(counter2+1) + ":4 "
+                        # svm_sec_str.write(str(counter3) + ":4 ")
+                    elif letter == 'E':
+                        sub_string += str(counter2+1) + ":3 "
+                        # svm_sec_str.write(str(counter3) + ":3 ")
+                    elif letter == 'C':
+                        sub_string += str(counter2+1) + ":2 "
+                        # svm_sec_str.write(str(counter3) + ":2 ")
+                    elif letter == '-':
+                        sub_string += str(counter2+1) + ":0 "
+                super_string += sub_string+"\n"
+        svm_sec_str.write(super_string)
+
+
+with open('svm_secondary_structure.train','rb') as semi_train:
+    lines = semi_train.readlines()
+    with open('svm.train','w') as train:
+        for line in lines[:-1]:
+            train.write(line)
+    with open('svm.predict','w') as predict:
+        predict.write("0 ")
+        predict.write(lines[-1][3:])
+
 with open('svm_secondary_structure.train','a') as svm_sec_str:
     string = ""
     super_string = ""
@@ -26,32 +76,3 @@ with open('svm_secondary_structure.train','a') as svm_sec_str:
         counter = 0
         for counter1, line in enumerate(lines):
             string += line[0:-1]
-
-    string.replace('\n','')
-    sub_string = ""
-    for counter3,letter in enumerate(string):
-        if letter == 'H':
-            sub_string += str(counter3) + ":4 "
-            # svm_sec_str.write(str(counter3) + ":4 ")
-        elif letter == 'E':
-            sub_string += str(counter3) + ":3 "
-            # svm_sec_str.write(str(counter3) + ":3 ")
-        elif letter == 'C':
-            sub_string += str(counter3) + ":2 "
-            # svm_sec_str.write(str(counter3) + ":2 ")
-        elif letter == '-':
-            sub_string += str(counter3) + ":0 "
-            # svm_sec_str.write(str(counter3) + ":0 ")
-    super_string+= sub_string+"\n"
-    svm_sec_str.write(super_string)
-
-with open('./svm_fasta','w') as svm_fas:
-    with open(sys.argv[2], "rb") as infile:
-        ini = infile.read().replace('H','G').replace('E','A')
-        svm_fas.write(ini)
-    svm_fas.write(">query.d\n")
-    with open("seq.sec_str", "rb") as infile:
-        ini = infile.read().replace('H','G').replace('E','A')
-        svm_fas.write(ini)
-
-# os.system('/home/primus/durgeshppi/tools/blast/clustalo'+' -i ./svm_fasta -o'+ ' ./output.txt'+ ' --outfmt'+ ' vie' + ' --force')
